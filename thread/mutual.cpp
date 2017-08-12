a6d038788d1c3c9790c02af836de4c097ad1fa75
jaye1944 <jaye1944@gmail.com>
2017-08-12 23:57:02 +0530

    change thread structure

--- thread/mutual.cpp
+++ thread/mutual.cpp
@@ -4,60 +4,64 @@
 #include <chrono>
 #include <thread>
 #include <mutex>
+#include <memory>
 #define NO_THREAD 10
+#define ELEMENTS 10000
 /*
-One thread write to map and other 10 threads read from map
-
-Thread - 7 8 => fake content
-Thread - 7 9 => fake content
-terminate called without an active exception
-./run.sh: line 8:  7468 Aborted                 ./$FILE
-
+5 thread write to map and other 5 threads read from map
 */
- 
-std::map<std::string, std::string> g_pages;
-std::mutex g_pages_mutex;
- 
-void save_page(const std::string &url)
+
+std::map<std::string, std::string> g_keyResult;
+std::mutex g_keyResult_mutex;
+
+void save_page(const std::string &url,int start)
 {
     // simulate a long page fetch
-    
+
     std::string result = "fake content";
-   // std::lock_guard<std::mutex> guard(g_pages_mutex);
-	for (int k = 0; k < 1000000 ; k++)
+    //std::lock_guard<std::mutex> guard(g_keyResult_mutex);
+	for (int k = 0; k < ELEMENTS ; k++)
 	{
 		std::string ke = std::to_string(k);
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
-		g_pages[ke] = result;
+		//std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		g_keyResult[ke] = result;
 	}
 }
 
 void get_page(const std::string &url,int tid)
 {
     // simulate a long page fetch
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    for (const auto &pair : g_pages) {
+    //std::lock_guard<std::mutex> guard(g_keyResult_mutex);
+   // std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    for (const auto &pair : g_keyResult) {
         std::cout << "Thread - " << tid << " " << pair.first << " => " << pair.second << '\n';
     }
 }
- 
- 
-int main() 
+
+
+int main()
 {
-    
+
     std::thread art[NO_THREAD];
-    
-    //write to map 
-    std::thread t1(save_page, "add to map");
-    
+
     //read from map
     for (int i = 0; i < NO_THREAD; ++i) {
-		art[i] = std::thread(get_page, "read from map",i);
+		if(i%2 != 0)
+		{
+			std::cout << "read i " << i << std::endl;
+			art[i] = std::thread(get_page, "read from map",i);
+		}
+		else
+		{
+			std::cout << "Write i " << i << std::endl;
+			art[i] = std::thread(save_page, "add to map",i);
+		}
     }
-    
+
     //join to main
     for (int i = 0; i < NO_THREAD; ++i) {
 		art[i].join();
 	}
- 
+	//t1.join();
+
 }
--- thread/run.sh
+++ thread/run.sh
@@ -5,12 +5,28 @@ LOG=valgrind.log
 LOGNAME=$DIR'_'$LOG
 
 g++ -std=c++11 -pthread -ggdb $SOURCE_FILE
-./$FILE
-rm $FILE
-#if [ "$#" -ne 1 ]; then
- # rm $FILE
-#fi
 
+if [ "$2" == "gd" ]; 
+then
+ 	gdb -ex=r $FILE
+else
+	./$FILE	
+fi
+
+if [ "$2" == "r" ]; 
+then
+ 	rm $FILE
+elif [ "$3" == "r" ]; 
+then
+	rm $FILE	
+fi
+
+#rm $FILE
+#if [ "$#" -ne 1 ]; then
+ #echo $#
+ #rm $FILE
+#fi
+#
 #if [ -e $FILE ]
 # then
 # valgrind -v --tool=memcheck --leak-check=full --log-file=logs/$LOGNAME ./$FILE
